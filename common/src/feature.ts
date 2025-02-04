3b534a7b536e4ef86e7bccf90a76a6aaf66496b5
FreshMag <pesco.magnani@gmail.com>
2025-02-04 14:44:01 +0100

    feat: dummy commit

--- common/src/feature.ts
+++ common/src/feature.ts
@@ -1,3 +1,7 @@
+/**
+ * Dummy feature
+ * @param date - Date to format
+ */
 export function formatDate(date: string | number | Date) {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     return new Date(date).toLocaleDateString(undefined, options);
