3c2a70e725266744c9b6c07209ed2b576ed5ee4b
FreshMag <pesco.magnani@gmail.com>
2025-02-04 15:30:47 +0100

    feat: fixed build issue

--- common/src/feature.ts
+++ common/src/feature.ts
@@ -3,6 +3,10 @@
  * @param date - Date to format
  */
 export function formatDate(date: string | number | Date) {
-  const options = { year: "numeric", month: "long", day: "numeric" };
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  };
   return new Date(date).toLocaleDateString(undefined, options);
 }
