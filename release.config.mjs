56446b421974d01a8e02e5f1b8557ffe8ca78e27
FreshMag <pescomagnani@gmail.com>
2025-02-06 18:20:21 +0100

    build: fixed release.config.mjs

--- release.config.mjs
+++ release.config.mjs
@@ -7,11 +7,14 @@ npm publish --access public
 import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
 
 config.plugins.push(
-    "@semantic-release/exec",
-    {
-      "publishCmd": publishCmd,
-    },
+    [
+      "@semantic-release/exec",
+      {
+        "publishCmd": publishCmd
+      }
+    ],
     "@semantic-release/github",
-    "@semantic-release/git",
-)
-export default config
+    "@semantic-release/git"
+);
+
+export default config;
