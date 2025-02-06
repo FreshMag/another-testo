c6c32b8dbb635b015198d93ca3c3c8c0b3bebaa3
FreshMag <pescomagnani@gmail.com>
2025-02-06 19:20:31 +0100

    fix: change publishCmd

--- release.config.mjs
+++ release.config.mjs
@@ -1,10 +1,9 @@
 const publishCmd = `
 cd common
 npm install
-echo $SEMANTIC_RELEASE_NEXT_VERSION
-npm version "$SEMANTIC_RELEASE_NEXT_VERSION"
+npm version ${process.env.SEMANTIC_RELEASE_NEXT_VERSION}
 npm publish --access public
-`
+`;
 
 import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
 
